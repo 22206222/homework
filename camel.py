cb6119e5b987ab74cf82094b20d9c479807f61d5
22206222 <muslima.jumaeva@icloud.com>
2024-12-01 21:49:30 +0000

    Sun, Dec 1, 2024, 11:49 PM +02:00

--- camel.py
+++ camel.py
@@ -1,1 +1,9 @@
-name = input("lease e")
+name = input("Please enter a variable name in Camel Case:")
+snake = ' '
+
+for i in range(len(name)):
+    if name[i].isupper():
+        snake = snake + '_' + name[i].lower()
+    else:
+        snake += name[i]
+print(snake)
