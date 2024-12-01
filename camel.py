be8bce166a60eca0f952ef57386de2167450be47
22206222 <muslima.jumaeva@icloud.com>
2024-12-01 22:17:49 +0000

    Mon, Dec 2, 2024, 12:17 AM +02:00

--- camel.py
+++
@@ -1,9 +0,0 @@
-name = input("Please enter a variable name in Camel Case:")
-snake = ' '
-
-for i in range(len(name)):
-    if name[i].isupper():
-        snake = snake + '_' + name[i].lower()
-    else:
-        snake += name[i]
-print(snake)
